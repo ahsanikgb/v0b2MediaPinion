24ae6de772500c8c09b759f299dda7395f37b945
wasi12 <wasi.sohail@gmail.com>
2019-01-30 12:37:51 +0500

    dependency function removed

--- DotnetcoreMediaPanion/ViewModels1/MyFirstDBContext.cs
+++ DotnetcoreMediaPanion/ViewModels1/MyFirstDBContext.cs
@@ -21,14 +21,7 @@ namespace DotnetcoreMediaPanion.ViewModels1
         public virtual DbSet<Services> Services { get; set; }
         public virtual DbSet<Slider> Slider { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:ahsanikgb.database.windows.net,1433;Initial Catalog=MyFirstDB;Persist Security Info=True;User ID=ahsanikgb;Password=12Arid2791;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
